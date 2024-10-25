8ff3548f9f7430faab24ee593603aedfee64dec9
allah <k23934926@gmail.com>
2024-10-25 17:26:46 +0300

    popa2

--- 1.js
+++ 1.js
@@ -1,9 +1,9 @@
 window.onload = function()
 {
-    const name = "Деев Леонид Русланович";
+    const name = "Юрин Николай Игоревич";
     const element = document.querySelector("#name");
     if (element)
     {
         element.textContent = name;
     }
-}
+}git
